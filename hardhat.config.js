f5caeaf25c0af998e5ae09937a9f43ca1a9cd864
Tushar Nagar <tusharrock146@gmail.com>
2025-02-26 23:56:13 +0530

    Update hardhat.config.js
    Update hardhat.config.js to include Sepolia network configuration and change solidity version.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/YOUR_ALCHEMY_API_KEY",
+      accounts: ["YOUR_PRIVATE_KEY"]
+    }
+  },
+  solidity: "0.8.19",
 };
